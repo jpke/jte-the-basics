4fca6cf4cec7c01c4a7363023e0d5a161e40b881
JP <21163267+jpke@users.noreply.github.com>
2021-04-28 11:51:29 -0400

    commit to save

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Following along with SDP learning lab https://boozallen.github.io/sdp-docs/learning-labs/1/index.html
+
+
+Completed learning-lab pipeline config and Dockerfile under `./learning-lab`
---
+++ learning-lab/pipeline_config.groovy
@@ -0,0 +1,39 @@
+libraries{
+    maven
+    sonarqube
+    ansible
+    splunk{
+        afterSteps = [ "static_code_analysis", "unit_test" ]
+    }
+    git
+}
+
+steps{
+    unit_test{
+        stage = "Unit Test"
+        image = "maven"
+        command = "mvn -v"
+    }
+}
+
+stages{
+    continuous_integration{
+        unit_test
+        build
+        static_code_analysis
+    }
+}
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
+
+keywords{
+    requiresApproval = true
+}
